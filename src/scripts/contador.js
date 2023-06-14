480a3e07c5b188e0218e1a97c3a736dd68c4747f
Marcos <marsanmar6291@gmail.com>
2023-06-14 08:11:15 -0600

    Contador

--- src/scripts/contador.js
+++ src/scripts/contador.js
@@ -1,38 +1,41 @@
-const COUNT_URL = "https://api.countapi.xyz";
-const NAMESPACE = "https://intermontrealdemanejo.com/";
-const KEY = "3702d928-0abb-11ee-be56-0242ac120002";
+import countapi from 'countapi-js';
+
+const NAMESPACE = 'https://intermontrealdemanejo.com/';
+const KEY = '3702d928-0abb-11ee-be56-0242ac120002';
 
 const counter = document.querySelectorAll('span');
 
-
 const getCount = async () => {
-    const response = await fetch(`${COUNT_URL}/get/${NAMESPACE}/${KEY}`);
-    const data = await response.json();
-      setValue(data.value);
-  };
-  
-  const incrementCount = async () => {
-    const response = await fetch(`${COUNT_URL}/hit/${NAMESPACE}/${KEY}`);
-    const data = await response.json();
-    setValue(data.value);
-  };
-  
-  const setValue = (num) => {
-    var str = num.toString().padStart(6, "0");
-    for (let index = 0; index < str.length; index++) {
-      const element = str[index];
-      counter[index].innerHTML = element;
-    }
-  };
-  
-  if (localStorage.getItem("hasVisited") == null) {
-    incrementCount()
-      .then(() => {
-        localStorage.setItem("hasVisited", "true");
-      })
-      .catch((err) => console.log(err));
-  } else {
-    getCount()
-      .catch((err) => console.log(err));
+  try {
+    const { value } = await countapi.get(NAMESPACE, KEY);
+    setValue(value);
+  } catch (error) {
+    console.log(error);
   }
-  
+};
+
+const incrementCount = async () => {
+  try {
+    const { value } = await countapi.hit(NAMESPACE, KEY);
+    setValue(value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const setValue = (num) => {
+  const str = num.toString().padStart(6, '0');
+  for (let index = 0; index < str.length; index++) {
+    counter[index].innerHTML = str[index];
+  }
+};
+
+if (localStorage.getItem('hasVisited') === null) {
+  incrementCount()
+    .then(() => {
+      localStorage.setItem('hasVisited', 'true');
+    })
+    .catch((err) => console.log(err));
+} else {
+  getCount().catch((err) => console.log(err));
+}
