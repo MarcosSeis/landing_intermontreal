d30b041a60ecf6d0fd360e43a937195e1ef87832
Marcos <marsanmar6291@gmail.com>
2023-06-14 09:41:05 -0600

    Contador

--- src/scripts/contador.js
+++ src/scripts/contador.js
@@ -1,19 +1,7 @@
-// Obtener el elemento del contador
-var contadorElemento = document.getElementById("contador");
 
-// Obtener el contador actual almacenado en el almacenamiento local
-var contadorActual = localStorage.getItem("contador");
-
-// Verificar si el contador existe en el almacenamiento local
-if (contadorActual) {
-  contadorActual = parseInt(contadorActual); // Convertir a número entero
-  contadorActual++; // Incrementar el contador en 1
-} else {
-  contadorActual = 1; // Establecer el contador en 1 si no existe
-}
-
-// Guardar el contador actualizado en el almacenamiento local
-localStorage.setItem("contador", contadorActual);
-
-// Mostrar el contador en la página
-contadorElemento.textContent = contadorActual;
+var sc_project=12891990; 
+var sc_invisible=0; 
+var sc_security="a30a786e"; 
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
